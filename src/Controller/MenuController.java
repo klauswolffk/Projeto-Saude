334e398b66ae131ce636cf425140969d69657dd5
Klaus Wolff <klauswolff2001@gmail.com>
2024-11-12 22:47:38 -0300

    Atualizações

--- src/Controller/MenuController.java
+++ src/Controller/MenuController.java
@@ -1,4 +1,17 @@
 package Controller;
 
+import View.MedicoView;
+
 public class MenuController {
+    MedicoView medicoview = new MedicoView();
+    public void principalcontroller(byte opcao) {
+        switch (opcao){
+            case 1:
+                medicoview.cadastrarMedico();
+                break;
+            case 2:
+                medicoview.listarMedico();
+                break;
+        }
+    }
 }
--- src/View/MedicoView.java
+++ src/View/MedicoView.java
@@ -1,25 +1,43 @@
 package View;
+import Controller.MedicoController;
 import Model.Medico;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MedicoView {
 
-    Scanner ler = new Scanner(System.in);
-    Medico medicoview = new Medico();
+    private Scanner ler = new Scanner(System.in);
 
-    private void cadastrarMedico(byte opcao) {
+    private MedicoController medicocontroller = new MedicoController();
+
+
+
+    public void cadastrarMedico() {
+        Medico medicoview = new Medico();
         System.out.println("Digite o nome do medico: ");
-        medicoview.setNome(ler.nextLine());
+        medicoview.setNome(ler.next());
         System.out.println("Digite a especialidade do médico: ");
-        medicoview.setEspecialidade(ler.nextLine());
+        medicoview.setEspecialidade(ler.next());
         System.out.println("Digite o CRM do médico: ");
         medicoview.setCrm(ler.nextInt());
         System.out.println("Digite o E-mail do médico: ");
-        medicoview.setEmail(ler.nextLine());
+        medicoview.setEmail(ler.next());
         System.out.println("Digite o telefone do médico: ");
-        medicoview.setTelefone(ler.nextLine());
-
-
-
+        medicoview.setTelefone(ler.next());
+        System.out.println(medicocontroller.registrarmedico(medicoview));
+        MenuView.mostrarMenuPrincipal();
+    }
+    public void listarMedico() {
+        ArrayList<Medico> listmedico = medicocontroller.listarMedicos();
+        for (Medico medico : listmedico) {
+            System.out.println("Nome: " + medico.getNome()
+                    + "\nEspecialidade: " + medico.getEspecialidade()
+                    + "\nCRM: " + medico.getCrm()
+                    + "\nE-mail: " + medico.getEmail()
+                    + "\nTelefone: " + medico.getTelefone());
+            System.out.println(" ");
+        }
+        MenuView.mostrarMenuPrincipal();
     }
 }
