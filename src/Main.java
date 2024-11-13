846c0b45c6f2dd19893dba26f0ba9cbcfb77cdf0
Klaus Wolff <klauswolff2001@gmail.com>
2024-11-12 22:47:38 -0300

    Atualizações

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import Controller.MenuController;
+import View.MenuView;
 
 public class Main {
     public static void main(String[] args) {
+        MenuController menucontroller = new MenuController();
+        MenuView menuview = new MenuView(menucontroller);
+        menuview.mostrarMenuPrincipal();
 
     }
 }
